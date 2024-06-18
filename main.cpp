d67db1aa7d694e84243d39bd7083ab7347c1d9f5
Ahmet Guzel <121121504+MyCodingSpace5@users.noreply.github.com>
2024-06-18 14:53:49 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <cstddef>
+#include <cstring>
+#include <iostream>
+#include <string>
+#include <typeinfo>
+
+struct Function
+{
+    std::string Name;
+    std::string ReturnType;
+};
+Function* calling_function(void* function_ptr_t[], int len)
+{
+    Function bozo[len];
+    for(int i = 0; i < len; i++)
+    {
+        const char* a;
+        a = (char*)(function_ptr_t);
+        Function b;
+        b.Name = a; 
+        b.ReturnType = typeid(function_ptr_t).name();
+        bozo[i] = b;
+    }
+    Function* m = (Function*)(bozo);
+    return m;
+}
+

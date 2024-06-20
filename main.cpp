08febd08184739dd6fbda4b6ad2a3e8b01ca27c1
Ahmet Guzel <121121504+MyCodingSpace5@users.noreply.github.com>
2024-06-19 21:56:00 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <string>
 #include <typeinfo>
-
+typedef void (*func_ptr)();
 struct Function
 {
     std::string Name;
@@ -21,6 +21,20 @@ Function* calling_function(void* function_ptr_t[], int len)
         b.ReturnType = typeid(function_ptr_t).name();
         bozo[i] = b;
     }
-    return (Function*)(bozo);
+    Function* m = bozo;
+    return m;
 }
+void** identify_function(int len)
+{
+    void* func;
+    void* lmao;
+    size_t bytes_increase = reinterpret_cast<char*>(&func) - reinterpret_cast<char*>(&lmao);
 
+    void** func_pointers = new void*[len];
+    for (int i = 0; i < len; ++i)
+    {
+        func_pointers[i] = reinterpret_cast<void*>(reinterpret_cast<char*>(&lmao) + i * bytes_increase);
+    }
+
+    return func_pointers;
+}

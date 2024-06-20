ccf62e4f14ea4b13b7bbcf32cb370598f36a59dd
Ahmet Guzel <121121504+MyCodingSpace5@users.noreply.github.com>
2024-06-19 22:06:56 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,36 +3,31 @@
 #include <iostream>
 #include <string>
 #include <typeinfo>
+
 typedef void (*func_ptr)();
-struct Function
-{
+
+struct Function {
     std::string Name;
     std::string ReturnType;
 };
-Function* calling_function(void* function_ptr_t[], int len)
-{
-    Function bozo[len];
-    for(int i = 0; i < len; i++)
-    {
-        const char* a;
-        a = (char*)(function_ptr_t);
-        Function b;
-        b.Name = a; 
-        b.ReturnType = typeid(function_ptr_t).name();
-        bozo[i] = b;
+
+extern "C" __declspec(dllexport) Function* calling_function(void* function_ptr_t[], int len) {
+    Function* bozo = new Function[len];
+    for (int i = 0; i < len; i++) {
+        const char* a = (char*)(function_ptr_t);
+        bozo[i].Name = a;
+        bozo[i].ReturnType = typeid(function_ptr_t).name();
     }
-    Function* m = bozo;
-    return m;
+    return bozo;
 }
-void** identify_function(int len)
-{
+
+extern "C" __declspec(dllexport) void** identify_function(int len) {
     void* func;
     void* lmao;
     size_t bytes_increase = reinterpret_cast<char*>(&func) - reinterpret_cast<char*>(&lmao);
 
     void** func_pointers = new void*[len];
-    for (int i = 0; i < len; ++i)
-    {
+    for (int i = 0; i < len; ++i) {
         func_pointers[i] = reinterpret_cast<void*>(reinterpret_cast<char*>(&lmao) + i * bytes_increase);
     }
 
